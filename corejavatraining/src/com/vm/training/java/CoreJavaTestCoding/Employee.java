d2aad9238b53645ea288029448b3691684b59ce8
SANTOSH <saisantoshtalatam@gmail.com>
2021-06-01 12:27:18 +0530

    Java Test

---
+++ corejavatraining/src/com/vm/training/java/CoreJavaTestCoding/Employee.java
@@ -0,0 +1,41 @@
+package com.vm.training.java.CoreJavaTestCoding;
+
+import java.util.*;
+public class Employee {
+	private String firstName;
+	private String lastName;
+	public Employee(String firstName, String lastName) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter first name :");
+		String firstname=sc.next();
+		System.out.println("Enter last name :");
+		String lastName=sc.next();
+		
+		try
+		{
+			if(firstname.isEmpty()&& lastName.isEmpty())
+			{
+				throw new NullPointerException("Entry missing");
+			}
+			if(firstname.length()<=3&&lastName.length()<=3)
+			{
+				throw new Exception("Please enter atleast 3 characters");
+			}
+		}
+		catch(NullPointerException e)
+		{
+			e.printStackTrace();
+		}
+		catch(Exception e1)
+		{
+			e1.printStackTrace();
+		}
+	}
+
+}
+
---
+++ corejavatraining/src/com/vm/training/java/CoreJavaTestCoding/SavingsAccount.java
@@ -0,0 +1,36 @@
+package com.vm.training.java.CoreJavaTestCoding;
+
+import java.util.*;
+import java.io.*;
+public class SavingsAccount {
+int balance;
+int interest;
+public SavingsAccount(int balance, int interest) {
+	super();
+	this.balance = balance;
+	this.interest = interest;
+}
+
+public void withDraw(int amount)
+{
+	if(amount>balance)
+	{
+		System.out.println("your balance is very low");
+	}
+	else
+	{
+	      balance-=amount;
+	}
+
+}
+public void deposit(int amount)
+{
+	balance+=amount;
+}
+
+public void display()
+{
+	System.out.println("Your Current balance is :"+balance);
+}
+}
+
